668505d46c1c115baf722c1fee9d1e0221d5cfbf
Dmitry Korniychuk <mitris.d@gmail.com>
2013-06-25 13:10:33 +0300

    Small fix

--- table-hover.js
+++ table-hover.js
@@ -1,4 +1,4 @@
-/* table-hover.js 1.0.0
+/* table-hover.js 1.0.1
  * http://github.com/mitris/table-hover
  * Copyright (c) 2013 Dmitry Korniychuk; Licensed MIT */
 ;
@@ -9,18 +9,18 @@
 			var defaults = {colClass: "hover"};
 			options = $.extend(defaults, options);
 			return this.each(function() {
-				$(this).off('mouseover mouseleave').on('mouseover mouseleave', 'th, td', function(e) {
+				$(this).off('mouseover mouseleave').on('mouseover mouseleave', 'th:not(.ignore), td:not(.ignore)', function(e) {
 					var row = $(this).closest('tr'), table = $(this).closest('table');
 					if (e.type == 'mouseover') {
 						if ($(this).closest('tbody').size()) {
 							row.addClass("hover");
 						}
-						table.find('tbody td:nth-child(' + ($(this).index() + 1) + ')').addClass('hover');
+						table.find('td:nth-child(' + ($(this).index() + 1) + ')').addClass('hover');
 					} else {
 						if ($(this).closest('tbody').size()) {
 							row.removeClass("hover");
 						}
-						table.find('tbody td:nth-child(' + ($(this).index() + 1) + ')').removeClass('hover');
+						table.find('td:nth-child(' + ($(this).index() + 1) + ')').removeClass('hover');
 					}
 				});
 			});
